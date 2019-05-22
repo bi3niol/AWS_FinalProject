4d24e438b928a3b7b72467e65180a1fa73b218b1
Piotr Storożenko <pitrosk@gmail.com>
2019-05-22 18:57:56 +0200

    Update classify_image.py

--- lambda-functions/api/classify_image.py
+++ lambda-functions/api/classify_image.py
@@ -9,31 +9,46 @@ IMAGE_FOLDER_PATH = Path("images")
 LOCAL_TMP_PATH = Path("/tmp")
 
 def save_to_local_file(filename, base64data):
-    barray = bytearray(base64data)
+    barray = bytearray(base64data, 'utf_8')
     filePath = LOCAL_TMP_PATH / filename
     with open(filePath,"wb") as file:
-        fh.write(base64.decodebytes(barray))
+        file.write(base64.decodebytes(barray))
 
     return filePath
 
 s3 = boto3.resource('s3')
 
+reko = boto3.client('rekognition')
+
 def lambda_handler(event,context):
-    model = event["body"]
+    model = json.loads(event["body"])
+    #model = event["body"]
     filename = model["filename"]
     imagedata = model["imagedata"]
     contenttype = model["contenttype"]
     
     file_destination = IMAGE_FOLDER_PATH / filename
 
-    localFile = save_to_local_file(filename,imagedata)
+    localFile: Path = save_to_local_file(filename,imagedata)
 
     unique_filename = f"{str(uuid.uuid4())}_{filename}"
 
     bucketFilePath = IMAGE_FOLDER_PATH / unique_filename
 
     s3.meta.client.upload_file(str(localFile),BUCKET_NAME, str(bucketFilePath))
-
+    
+    response = reko.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': BUCKET_NAME,
+                'Name': str(bucketFilePath)
+            }
+        },
+        MaxLabels=123
+    )
+    #print(response)
+    
+    
     return {
-        'body' : str(bucketFilePath)
+        'body' : "My Job done"
         }
